34421734646ed859054e56360f7f4a0503b324ae
Paco KIFOUETO <pacochristiangracekifoueto@gmail.com>
2023-09-30 20:50:14 +0100

    task

--- w02-task (2)/scripts/edit.js
+++ w02-task (2)/scripts/edit.js
@@ -78,29 +78,71 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-let fullName = 'Paco KIFOUETO';
-let currentYear = "2023";
-let profilePicutre = `images/Paco.jpg`;
+// let fullName = 'Paco KIFOUETO';
+// let currentYear = "2023";
+// let profilePicutre = `images/Paco.jpg`;
 
-/* Step 3 - Element Variables */
+// /* Step 3 - Element Variables */
+// const nameElement = document.getElementById('name');
+// const foodElement = document.getElementById('food');
+// const yearElement = document.querySelector('#year');
+// const imageElement = document.querySelector('img');
+
+// /* Step 4 - Adding Content */
+// nameElement.innerHTML = `<strong>${fullName}</strong>`;
+// yearElement.textContent= currentYear;
+// imageElement.setAttribute('src', profilePicutre);
+// imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+
+// /* Step 5 - Array */
+// let favoriteFoods = ['Saka saka', 'Beans', 'rice']
+// let favFood = ['Peanut sauce'];
+// favoriteFoods.push(favFood);
+// foodElement.innerHTML += `<br>${favoriteFoods}`;
+// favoriteFoods.splice(0, 1);
+// foodElement.innerHTML += `<br>${favoriteFoods}`;
+// favoriteFoods.pop();
+// foodElement.innerHTML += `<br>${favoriteFoods}`;
+
+
+// The edited one trying to understand what was wrong with my code.
+
+
+let fullName = 'Paco KIFOUETO';
+// let currentYear = new Date().getFullYear();
+let currentYear = "2023";
+let profilelPicture = 'images/Paco.jpg';
+// let profilelPicture = 'Paco.jpg';
+
+
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
+// const imageElement = document.querySelector("img[src = 'images/Paco.jpg']");
 const imageElement = document.querySelector('img');
 
-/* Step 4 - Adding Content */
+
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
-yearElement.textContent= currentYear;
-imageElement.setAttribute('src', profilePicutre);
-imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+yearElement.textcontent = currentYear;
+imageElement.setAttribute('src', profilelPicture);
+// imageElement.setAttribute('alt', 'Profile image of ${fullName}');
 
 
-/* Step 5 - Array */
-let favoriteFoods = ['Saka saka', 'Beans', 'rice']
-let favFood = ['Peanut sauce'];
-favoriteFoods.push(favFood);
+let favoriteFood = ['Saka saka', 'Beans and rice', 'eggs'];
+// let favoriteFoods = ['Peanut sauce'];
+let favoriteFoods = 'Peanut sauce';
+// const favorites = favoriteFood.joint(',');
+favoriteFood.push(favoriteFoods);
+foodElement.innerHTML += `<br>${favoriteFood}`;
 foodElement.innerHTML += `<br>${favoriteFoods}`;
-favoriteFoods.splice(0, 1);
-foodElement.innerHTML += `<br>${favoriteFoods}`;
-favoriteFoods.pop();
-foodElement.innerHTML += `<br>${favoriteFoods}`;
+// favoriteFood.splice(0, 1);
+const removeFirst = favoriteFood.shift();
+foodElement.innerHTML += `<br>${favoriteFood}`;
+
+favoriteFood.push(favoriteFoods);
+foodElement.innerHTML += `<br>${favoriteFood}`;
+favoriteFood.pop();
+foodElement.innerHTML += `<br>${favoriteFood}`;
+
+// favoriteFood.push(favoriteFoods);
---
+++ w02-task (2)/scripts/w03-task.js
@@ -0,0 +1,51 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add (number1, number2) {
+    return number1 + number2;
+}
+
+function addNumbers () {
+    let addNumber1 = Number(document.querySelector('#add1').value);
+    let addNumber2 = Number(document.querySelector('#add2').value);
+    document.querySelector('#sum').value = add(addNumber1, addNumber2)
+}
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+
+/* Function Expression - Subtract Numbers */
+
+let substract = function(number1, number2) {
+    return number1 - number2;
+}
+
+function substractNumbers () {
+    let substractNumber1 = Number(document.querySelector('#subtract1').value);
+    let substractNumber2 = Number(document.querySelector('#subtract2').value);
+    let substractNumber3 = Number(document.querySelector('#substractNumbers').value);
+    let substractNumber4 = Number(document.querySelector('#difference').value);
+    document.querySelector('#subtractNumbers').value = substractNumbers(substractNumber1, substractNumber2)
+    document.querySelector('#subtractNumbers').value = substractNumbers(substractNumber3, substractNumber4)
+}
+document.querySelector('#substractNumbers').addEventListener('click', substractNumbers);
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
